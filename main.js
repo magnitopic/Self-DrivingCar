8c666b00e52a80ceba43564efbe1b2d39ccc4d39
magnitopic <magnitopic@gmail.com>
2022-05-27 12:04:06 +0200

    Save car brains

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const N = 100;
+const N = 1000;
 const cars = generataCars(N);
 let bestCar = [0];
 if (localStorage.getItem("bestBrain")) {
