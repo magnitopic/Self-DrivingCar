7b8510634110760313d6954ad1ef0341aca673fa
magnitopic <magnitopic@gmail.com>
2022-05-27 23:48:54 +0200

    mutations

--- main.js
+++ main.js
@@ -7,17 +7,29 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const N = 1000;
+const N = 100000;
 const cars = generataCars(N);
 let bestCar = [0];
 if (localStorage.getItem("bestBrain")) {
-	bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+	for (let i = 0; i < cars.length; i++) {
+		cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+		if (i != 0) {
+			NeuralNetwork.mutate(cars[i].brain, 0.1);
+		}
+	}
 }
 
 const traffic = [
 	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
 	new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
 	new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(0), -900, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(1), -1100, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(0), -1300, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(1), -1300, 30, 50, "DUMMY", 2),
 ];
 
 animate();
